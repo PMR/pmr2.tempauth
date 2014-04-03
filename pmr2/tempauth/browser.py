9d520bdff13a2addd524028c8d804889b07bf8fb
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-04-03 18:59:27 +1300

    Store the access token during composition
    - render now only renders that stored token.

--- pmr2/tempauth/browser.py
+++ pmr2/tempauth/browser.py
@@ -27,19 +27,21 @@ class ContextTempAuthRequestForm(BrowserPage):
             raise Unauthorized('invalid content type')
 
         mt = getToolByName(self.context, 'portal_membership')
-        self.target = self.context.absolute_url()
-        self.user = mt.getAuthenticatedMember().id
+        authutil = zope.component.getAdapter(getSite(), ITemporaryAuth)
+
+        target = self.context.absolute_url()
+        user = mt.getAuthenticatedMember().id
+        key = authutil.generateAccessFor(user, target)
+
+        self.access = {
+            'user': user,
+            'key': key,
+            'target': target,
+        }
 
     def render(self):
-        authutil = zope.component.getAdapter(getSite(), ITemporaryAuth)
-        key = authutil.generateAccessFor(self.user, self.target)
-
         self.request.response.setHeader('Content-type', self.content_type)
-        return json.dumps({
-            'user': self.user,
-            'key': key,
-            'target': self.target,
-        })
+        return json.dumps(self.access)
 
     def __call__(self):
         self.update()
