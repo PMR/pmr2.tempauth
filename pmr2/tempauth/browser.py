b97e6535b69b478055e32cbb3e19b7825dd13cee
Tommy Yu <tommy.yu@auckland.ac.nz>
2017-01-06 23:31:22 +1300

    Disable plone.protect CSRF protection
    - Since this form is used from a webservice context, it is irrelevant.

--- pmr2/tempauth/browser.py
+++ pmr2/tempauth/browser.py
@@ -8,6 +8,12 @@ from zope.publisher.browser import BrowserPage
 from AccessControl import Unauthorized
 from Products.CMFCore.utils import getToolByName
 
+try:
+    from plone.protect.interfaces import IDisableCSRFProtection
+    PLONE_PROTECT = True
+except ImportError:
+    PLONE_PROTECT = False
+
 from pmr2.tempauth.interfaces import ITemporaryAuth
 
 
@@ -25,6 +31,9 @@ class ContextTempAuthRequestForm(BrowserPage):
 
         if self.request.getHeader('Content-type') != self.content_type:
             raise Unauthorized('invalid content type')
+
+        if PLONE_PROTECT:
+            zope.interface.alsoProvides(self.request, IDisableCSRFProtection)
 
         mt = getToolByName(self.context, 'portal_membership')
         authutil = zope.component.getAdapter(getSite(), ITemporaryAuth)
